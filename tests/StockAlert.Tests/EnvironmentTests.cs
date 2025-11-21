bea3f4743e57c74bb5e0dacad37252f2a5352310
Julia Mequilini <jujulrocha@gmail.com>
2025-11-21 12:09:53 -0300

    +xunit

---
+++ tests/StockAlert.Tests/EnvironmentTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace StockAlert.Tests
+{
+    public class EnvironmentTests
+    {
+        [Fact]
+        public void Environment_Is_Working()
+        {
+            Assert.True(true);
+        }
+    }
+}
